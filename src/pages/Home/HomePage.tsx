b0e5f15624ad999230337d81079c1d7545957d3e
Na3ar-17 <gavruk2017@gmail.com>
2024-01-07 15:57:44 +0200

    trying to fix problem with Title component N2

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -2,11 +2,7 @@ import { NextPage } from 'next'
 import styles from './Home.module.scss'
 import Title from '../../components/display/Title/Title'
 const HomePage: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Title />
-    </div>
-  )
+  return <div className={styles.container}>{/* <Title /> */}</div>
 }
 
 export default HomePage
